3dbb02ae8873e11298a3e048dfcfd17ef812125d
Sander Fernandez <sanderfernandez0812@gmail.com>
2024-11-29 20:05:59 -0400

    other commit

--- tests/test_index.py
+++ tests/test_index.py
@@ -3,4 +3,4 @@ import requests
 def test_index_page():
     response = requests.get("http://localhost:9090")
     assert response.status_code == 200
-    assert "Hola Mundo" in response.text
+    assert "Hello World" in response.text
